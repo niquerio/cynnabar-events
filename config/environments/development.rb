6a305faa1bedb971bd62c4c6dfeddcbd190cc3a0
Monique Rio <nique.rio@gmail.com>
2018-12-10 11:04:14 -0500

    setup devise, cancancan, and bootstrap_form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  #For Devise
+  config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3333 }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
